b059c23648dbedafd8e0b9b27ed06be7358e54a7
ashleigh090990 <ashleighmaund@gmail.com>
2015-08-21 13:34:30 +0100

    styled a bit - spoilers currently hardcoded, changing it so that they can be added

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 $(".spoiler").hide();
 
-$("#spoilerButton").click(function() {
-  $(".spoiler").show('slow')
+$(".eachDiv").append("<button class='spoilerButton genericBorder buttonBorder'>Reveal Spoiler</button>");
+
+$(".spoilerButton").click(function() {
+  $(this).prev().show();
+  (this).remove('slow');
 });
+
+// $("#addSpoiler").click(function() {
+//   $('.containingDiv').append("<div class='genericBorder eachDiv'><span class='spoiler'></span></div>");
+//   $(".spoiler").hide();
+//   $(".eachDiv").append("<button class='spoilerButton genericBorder buttonBorder'>Reveal Spoiler</button>");
+// });
